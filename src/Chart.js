34c645bcd6d43aa334c3479feea1a1344d50f9d3
layra.costa <layra.costa@pipetech.com.br>
2022-06-01 15:59:11 -0300

    primeira carga componentes react

---
+++ src/Chart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Title from './component/Title';
+
+export default function Chart() {
+
+  return (
+    <React.Fragment>
+      <Title>TESTE</Title>
+    </React.Fragment>
+  );
+}
